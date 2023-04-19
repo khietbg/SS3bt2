cb893eb71b72205161282105348730937894470a
minhkhiet14151993 <119550390+minhkhiet14151993@users.noreply.github.com>
2023-04-19 11:45:07 +0700

    Add files via upload
    ss3bt2

---
+++ SS3bt2.sql
@@ -0,0 +1,63 @@
+
+-- use SS3bt2;
+-- create table customer(
+-- cid int primary key auto_increment,
+-- cname varchar(255) not null,
+-- cage int not null
+-- );
+
+-- create table product(
+-- pid int primary key auto_increment,
+-- pname varchar(255) not null,
+-- pPrice float not null
+-- );
+
+-- create table `order`(
+-- oid int primary key auto_increment,
+-- odate date not null,
+-- ototalprice float not null,
+-- cid int,
+-- foreign key(cid) references customer(cid)
+-- );
+
+-- create table orderdetail(
+-- odi int,
+-- foreign key(odi) references `order`(oid),
+-- pid int,
+-- foreign key(pid) references product(pid)
+-- );
+-- insert into customer(cname, cage ) values
+-- ("Minh Quan", 10),
+-- ("Ngoc Oanh", 20),
+-- ("Hong Ha", 50);
+
+-- insert into product(pname, pPrice) values
+-- ("May giat", 3),
+-- ("Tu lạnh", 5),
+-- ("Dieu hoa", 7),
+-- ("Quat", 1),
+-- ("Bep dien", 2);
+-- insert into `order`(cID,oDate,oTotalPrice) values
+-- (1,"2006-3-21",0),
+-- (2,"2006-3-23",0),
+-- (1,"2006-3-16",0)
+-- ;
+-- select * from`order`;
+-- select * from product;
+-- insert into orderdetail(odi, pid, odQTY) values
+-- (4,1,3),
+-- (5,3,7),
+-- (4,4,2),
+-- (5,1,1),
+-- (6,1,8),
+-- (5,5,4),
+-- (4,3,3);
+ -- •	Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
+ -- select oID,odate,oTotalPrice from `order`;
+ -- •	Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
+ -- select c.cname,p.pname from ((customer c join `order` o on c.cid = o.cid) join orderdetail od on o.oid = od.odi) join product p on p.pid = od.pid;
+ -- •	Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào
+-- select * from customer  where cid not in(select o.cid from `order` o);
+-- •	Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn (giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng
+-- xuất hiện trong hóa đơn. Giá bán của từng loại được tính = odQTY*pPrice)
+select o.cid,o.oDate,od.odQTY*p.pprice as `tong tien` from (`order` o join orderdetail od on o.oid = od.odi) join product p on od.pid= p.pid  
